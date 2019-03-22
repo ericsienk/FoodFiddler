06b5d583894546cbf2b9ac5b0bb332029cc3d5d5
ericsienk <eric.sienk@temple.edu>
2019-03-22 15:51:58 -0400

    mvp preview image on edit recipe

--- docs/common/directives/imgur-loader.js
+++ docs/common/directives/imgur-loader.js
@@ -5,42 +5,71 @@
             return {
                 restrict: 'A',
                 link: function (scope, element, attrs) {
+                    var WATCH_URL = false,
+                        LOADING_IMG = false;
+
+                    function configureImgUrl(url) {
+                        if(attrs.imgurLoader.indexOf("i.imgur.com") > -1) {
+                            return attrs.max ? url : url.replace(/(.+)(\.\w{3}$)/, "$1l$2");
+                        } else {
+                            return url;
+                        }
+                    }
+
+                    var tmpImg;
+                    function loadImg(url) {
+                        url = configureImgUrl(url);
+                        tmpImg = angular.element(new Image());
+                        tmpImg.attr('src', url);
+
+                        tmpImg.on('load', function () {
+                            if(!WATCH_URL) {
+                                element[0].style.transition = '.5s filter';
+                            }
+
+                            element[0].style.background = 'url("' + url + '") ' + (attrs.options ? attrs.options : 'no-repeat center');
+                            element[0].style.backgroundSize = 'cover';
+                            element[0].style.filter = '';
+                            if(!WATCH_URL) {
+                                element.removeAttr("imgur-loader");
+                            }
+                        }).on('error', function () {
+                            element[0].style.filter = '';
+                            if(!WATCH_URL) {
+                                element.removeAttr("imgur-loader");
+                            } else {
+                                element[0].style.background = '';
+                            }
+                        });
+
+                        if (!tmpImg[0].complete) {
+                            element[0].style.filter = 'blur(22px)';
+                        }
+                    }
+
+                    function lazyLoadImg() {
+                        if(LOADING_IMG) {
+                            lazyLoaderService.unsubscribe(element[0], 'imgurLoader');
+                        } else {
+                            LOADING_IMG = true;
+                        }
+
+                        lazyLoaderService.subscribe(element[0], 'imgurLoader', function(data) {
+                            lazyLoaderService.unsubscribe(element[0], 'imgurLoader');
+                            loadImg(attrs.imgurLoader);
+                        });
+                    }
 
                     scope.$on('$destroy', function() {
                         lazyLoaderService.unsubscribe(element[0], 'imgurLoader');
                     });
 
-                    lazyLoaderService.subscribe(element[0], 'imgurLoader', function(data) {
-                        lazyLoaderService.unsubscribe(element[0], 'imgurLoader');
-
-                        var url = attrs.imgurLoader;
-
-                        function loadImg(url) {
-                            var tmpImg = angular.element(new Image());
-                            tmpImg.attr('src', url);
-
-                            tmpImg.on('load', function () {
-                                element[0].style.transition = '.5s filter';
-                                element[0].style.background = 'url("' + url + '") ' + (attrs.options ? attrs.options : 'no-repeat center');
-                                element[0].style.backgroundSize = 'cover';
-                                element[0].style.filter = '';
-                                element.removeAttr("imgur-loader");
-                            }).on('error', function () {
-                                element[0].style.filter = '';
-                                element.removeAttr("imgur-loader");
-                            });
-
-                            if(!tmpImg[0].complete) {
-                                element[0].style.filter = 'blur(22px)';
-                            }
-                        }
-
-                        if(attrs.imgurLoader.indexOf("i.imgur.com") > -1) {
-                            loadImg(attrs.max ? url : url.replace(/(.+)(\.\w{3}$)/, "$1l$2"));
-                        } else {
-                            loadImg(url);
-                        }
-                    });
+                    if(attrs.autoWatch === 'true') {
+                        WATCH_URL = true;
+                        scope.$watch(function() { return attrs.imgurLoader }, lazyLoadImg);
+                    } else {
+                        lazyLoadImg();
+                    }
                 }
             };
         });
